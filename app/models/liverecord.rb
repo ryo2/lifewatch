4c9a0f96bba828201cb77ea76235c1a9cfd7ed3e
deeeki <deeeki+gh@gmail.com>
2012-08-13 00:08:33 +0900

    Refactor timeline implementation

--- app/models/liverecord.rb
+++ app/models/liverecord.rb
@@ -3,46 +3,31 @@ class Liverecord < ActiveRecord::Base
   acts_as_taggable
   belongs_to :user
   belongs_to :task
-  
-  def self.get_timeline date, start_time = 8, end_time = 24
-    timeline = Array.new
-    
-    0.upto(23) do |time|
-      @i = 0
 
-      self.where(:start => date..date+1).all.each do |r|
-        if r.start.hour  == time
-          line = {
-            model: r,
-            tag: r.tags[0],
-            task: r.task.name,
-            start: r.start.strftime("%H:%M"),
-            end: r.end.strftime("%H:%M"),
-            pass: self.s_to_hm(r.end - r.start)
-          }
-          line[:time] = time if @i == 0
-          timeline << line
-          @i += 1
-        end
-      end
-      
-      if time >= start_time and time <= end_time
-        while @i < 2
-          line = Hash.new
-          line[:time] = time if @i == 0
-          timeline << line
-          @i += 1
-        end
-      end
-
+  def self.timeline date, start_hour = 8, end_hour = 24
+    #initialize
+    timeline = {}
+    start_hour.upto(end_hour) do |hour|
+      timeline[hour] = []
     end
-    timeline
-  end
-  
-  def self.s_to_hm s
-    hour = s/3600
-    minute = (s%3600)/60
-    pass = "%02d" % hour + ':' + "%02d" % minute
+    #insert real data
+    self.where(:start => date..(date + 1.day)).each do |l|
+      timeline[l.start.hour] << l
+    end
+    #insert dummy data
+    start_hour.upto(end_hour) do |hour|
+      timeline[hour].count.upto(1) do
+        timeline[hour] << self.new
+      end
+    end
+
+    timeline # Hash: key is hour, value is array of liverecords
   end
 
+  def pass
+    return if !self.start || !self.end
+    hour = (self.end - self.start) / 3600
+    minute = ((self.end - self.start) % 3600) / 60
+    "#{'%02d' % hour}:#{'%02d' % minute}"
+  end
 end
